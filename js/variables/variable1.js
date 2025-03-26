35902df98ab9ef8ae868a0a5923dcd1494e0dfdb
Osorabrian <osorabrian@gmail.com>
2025-03-26 14:51:12 -0700

    create variable js folder holding notes to javascriot

---
+++ js/variables/variable1.js
@@ -0,0 +1,39 @@
+"use strict"
+/* use strict
+is used to run javascript code in strict mode
+it makes previously bad accepted syntax into real errors
+*/
+
+/*
+defining variables
+variable is a named storage for data
+~rules~
+a) only made of letters, numbers and symols $ and _.
+b) cannot start with a number.
+c) we use camelCase e.g myName, currentUserEmail.
+d) non latin letters are allowed but not recommended.
+e) use human readable name.
+f) do not use abbreviations.
+g) make names maximally descriptive and concise.
+*/
+
+
+// let keyword is used to declare varibles whose values can change
+let name = "Brian";
+console.log(name)
+
+// const keyword is used to declare variables whose value cannot change
+const lastName = "Isaboke"
+
+// capitalized constants are used for hard coded values that are known before execution
+const  COLOR_ORANGE = "#ff0000"
+
+let admin
+let fame
+
+fame = "John"
+admin = fame
+
+alert(admin)
+
+
