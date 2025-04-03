314fb6b8dc40266e80321fec2fa1fa777406e8d1
Osorabrian <osorabrian@gmail.com>
2025-04-02 23:33:52 -0700

    created a prompt.js file illustrating how prompts work

---
+++ js/prompt/prompt.js
@@ -0,0 +1,5 @@
+// prompt is a function that shows a modal window that asks for user input, an input field and the buttons OK or Cancel.
+let result = +prompt("Enter a number")
+
+// this will return the result in a modal window.
+alert(result)
