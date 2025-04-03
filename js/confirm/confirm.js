9eaa3d7dc175bb216dae5db74a15982740d8d06a
Osorabrian <osorabrian@gmail.com>
2025-04-02 23:51:09 -0700

    create notes on confirm.js

---
+++ js/confirm/confirm.js
@@ -0,0 +1,6 @@
+// Confirm function shows a modal window with a text message and two button OK and cancel.
+// If the user presses OK the result is TRUE but if it is Cnacel the result is FALSE.
+
+let result = confirm("Are you Brian?")
+
+alert(result)
--- js/prompt/prompt.js
+++ js/prompt/prompt.js
@@ -1,5 +1,6 @@
 // prompt is a function that shows a modal window that asks for user input, an input field and the buttons OK or Cancel.
-let result = +prompt("Enter a number")
+// a prompt function accepts two arguments, a text message and initial value for the input field.
+let result = +prompt("Enter a number", 12)
 
 // this will return the result in a modal window.
 alert(result)
