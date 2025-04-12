e2c14b2b691bfcc9ae79331cf016abf9f96bbecf
Osorabrian <osorabrian@gmail.com>
2025-04-12 00:39:04 -0700

    publish ed notes on for loop

--- js/loops/loops.js
+++ js/loops/loops.js
@@ -7,7 +7,6 @@ there are several loops in js
 */
 
 //while loop - executes a code of block if condition is met while(condition){//code}
-
 let age = 18
 while(age < 25){ //will check if condition is met if truthy it executes the code block below
     console.log("You are still underage")
@@ -19,6 +18,25 @@ while(age < 25){ //will check if condition is met if truthy it executes the code
 //     console.log("Brian")
 // } this will loop forever (infinite loop)
 
+//do...while loops - exceutes a block of code while, the while statement is true.
+let myAge = 16
+do{
+    console.log("You cannot drink and drive.")
+    myAge++
+}while(myAge < 21)
 
+
+//for loop - for(begin; condition; step)
+for(let a = 0; a < 10; a++){
+    console.log("Hello")
+}
+
+/*
+In the for loop 
+begin - executes upon entering the loop
+conditon - checked again before any iteration
+body - executed while condition is truthy
+step - executes after the body on each iteration
+*/
 
 
