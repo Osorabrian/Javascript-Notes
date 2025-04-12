0637f3ee0f0fb9992ec14038ac910e99544e52f0
Osorabrian <osorabrian@gmail.com>
2025-04-12 00:05:25 -0700

    add notes on return statement and function good practices

--- js/functions/function.js
+++ js/functions/function.js
@@ -1,11 +1,12 @@
 /*
-Functins are the main building blocks of a program
+Functi0ns are the main building blocks of a program
 to create we use a function declaration
 */
 
 function fxName(parameter1, parameter2){
     //code block
-    return (parameter1 + parameter2)
+    return (parameter1 + parameter2)// return - returns a value back in the calling code.when the execution reaches the return statement the execution stops.
+    //a function with an empty return or without one returns undefined.
 }   
 
 fxName(2,3) //calling a function in order to invoke it
@@ -13,3 +14,10 @@ fxName(2,3) //calling a function in order to invoke it
 //parameter is a variable that is listed in the function declaration
 //argument is a value passed to a function when it is called
 console.log(fxName("Brian", "Osora"))
+
+/*
+ADVICE
+functions are actions. So the name should be a verb, brief, as accurate as possible and describe what the function does
+one function should perform one action
+a function should be short and do exactly one thing.
+*/
