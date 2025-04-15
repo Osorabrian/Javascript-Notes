4ffc13314223b069f857af5f404c5084d125b000
Osorabrian <osorabrian@gmail.com>
2025-04-14 21:05:17 -0700

    add notes on function expresiions

---
+++ js/functions/expresiions.js
@@ -0,0 +1,15 @@
+/*
+Function Expressions
+This is a function created in the middle of any expression
+let func = function(arg1, arg2...argN) {body}
+*/
+
+let greeting = function(name) {
+    return name
+}
+console.log( greeting("Brian") )
+
+//or 
+
+let hello = function(name) { return name }
+console.log(hello("John"))
